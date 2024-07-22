38021c5257d994135078f24a7c6a14d5d76d5969
OBoladeras <oriolboladeras@gmail.com>
2024-07-22 17:11:15 +0200

    Changes to add webpage icon

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import secrets
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
 limiter = Limiter(get_remote_address, app=app)
 
+
 @app.errorhandler(429)
 def ratelimit_handler(e):
     return render_template("429.html"), 429
 
+
 @app.route("/favicon.ico")
 def favicon():
     return redirect(url_for("static", filename="favicon.ico"), code=302)
-
 
 
 @app.route('/')
@@ -29,7 +30,7 @@ def login():
 
 
 @app.route('/login', methods=['POST'])
-@limiter.limit("5 per minute") # Limit to 5 login attempts per minute 
+@limiter.limit("5 per minute")  # Limit to 5 login attempts per minute
 def login_post():
     username = request.form['username']
     password = request.form['password']
